0efa37273504eb0a758869cad9eaae77c7070cf1
abdurrahmanvurgun <wvurgun47@gmail.com>
2025-06-11 16:14:49 +0300

    Change UI Kit and Save responsive button

---
+++ app/app.tsx
@@ -0,0 +1,32 @@
+import MyKeyboard from '@/components/MyKeyboard';
+import { ThemeContext } from '@/context/ThemeContext';
+import { myColors } from '@/styles/Colors';
+import { useState } from 'react';
+import { SafeAreaView, StyleSheet, Switch } from 'react-native';
+
+
+
+
+export default function App() {
+  const [theme, setTheme] = useState('light');
+  return (
+    <ThemeContext.Provider value={theme}>
+      <SafeAreaView style={theme === 'light' ? styles.container : [styles.container, {backgroundColor: 'black'}]}>
+        <Switch
+          value={theme === 'dark'}
+          onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        />
+        <MyKeyboard />
+      </SafeAreaView>
+    </ThemeContext.Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: myColors.light,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+});
